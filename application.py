49ad8a198b171d6162202d8dd6c210273210a083
isimanovich <tak_netak@hotmail.com>
2017-03-06 12:39:17 -0800

    account page added

--- application.py
+++ application.py
@@ -3,7 +3,7 @@ import time
 import boto3, os, requests, shutil
 from boto3.dynamodb.conditions import Key, Attr
 from flask import Flask
-from flask import render_template, url_for, request, jsonify, Markup, flash
+from flask import render_template, redirect, url_for, request, jsonify, Markup, flash
 
 application = Flask(__name__)
 
@@ -15,6 +15,13 @@ access_key = 'AKIAJSVUOAS23R7X3XZA'
 secret_key = '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'
 dynamodb = boto3.resource('dynamodb', region_name='us-west-2')
 table = dynamodb.Table('490final-userinfostore')
+
+
+# this function will add contact to the contacts_database
+@application.route("/add_contact", methods=['POST', 'GET'])
+def add_contact():
+    print("GOT HERE")
+    return redirect("/account_page")
 
 
 def createTable():
@@ -62,6 +69,12 @@ def main():
 
 
 # method to render login page
+@application.route('/account_page', methods=['GET', 'POST'])
+def account_page():
+    return render_template('account.html')
+
+
+# method to render login page
 @application.route('/login_page', methods=['GET', 'POST'])
 def login_page():
     return render_template('login.html')
@@ -79,12 +92,6 @@ def login():
 # method to render sign up page
 @application.route('/signup', methods=['GET', 'POST'])
 def signup():
-    createTable()
-    return render_template('signup.html')
-
-# method to render register page
-@application.route('/register', methods=['GET', 'POST'])
-def register():
     createTable()
     email_input = request.form.get("email")
     password_input = request.form.get("password")
@@ -110,17 +117,39 @@ def register():
 
     return render_template('login.html')
 
+# # method to render register page
+# @application.route('/register', methods=['GET', 'POST'])
+# def register():
+#     createTable()
+#     email_input = request.form.get("email")
+#     password_input = request.form.get("password")
+#     password_verify_input = request.form.get("password_verify")
+#     first_name_input = request.form.get("first_name")
+#     last_name_input = request.form.get("last_name")
+#
+#     if password_input != password_verify_input:
+#         password_error = Markup("<p> Passwords provided do not match, please try again.</p>")
+#         flash(password_error)
+#         return render_template('signup.html')
+#
+#     response = table.query(KeyConditionExpression=Key('email').eq(email_input))
+#     items = response['Items']
+#     if len(items) > 0:
+#         dup_email_error = Markup("<p> The email is already in use with our services, please log in<p>")
+#         flash(dup_email_error)
+#         return render_template('signup.html')
+#
+#     dict={'email': email_input, 'password': password_input,
+#         'first_name': first_name_input, 'last_name': last_name_input}
+#     table.put_item(Item=dict)
+#
+#     return render_template('login.html')
+
 
 # method to render signup page
 @application.route('/signup_page', methods=['GET', 'POST'])
 def signup_page():
     return render_template('signup.html')
-
-
-# method to render account page
-@application.route('/account_page', methods=['GET', 'POST'])
-def account_page():
-    return render_template('account.html')
 
 
 if __name__ == '__main__':
