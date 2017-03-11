88fe0ec4d4548935c3106341c01f2d8b08240ca5
isimanovich <tak_netak@hotmail.com>
2017-03-11 15:28:20 -0800

    alert for deleting messages

--- static/javascr.js
+++ static/javascr.js
@@ -1,3 +1,6 @@
-function sendData() {
-    alert()
+function confirmSubmit() {
+  if (confirm("Are you sure you want to delete these contacts?")) {
+    document.getElementById("FORM_ID").submit();
+  }
+  return false;
 }
